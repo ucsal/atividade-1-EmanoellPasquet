16f129a609a1d73930e3a81a83b8d71ff577a99c
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-04-04 19:07:05 -0300

    Done!

--- app/src/main/java/br/com/mariojp/exercise1/MainActivity.java
+++ app/src/main/java/br/com/mariojp/exercise1/MainActivity.java
@@ -2,19 +2,35 @@ package br.com.mariojp.exercise1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String INPUT = "INPUT";
+    private TextView userInput, userOutput;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userInput = findViewById(R.id.editNome);
+        userOutput = findViewById(R.id.labelMensagem);
+        if (savedInstanceState != null){
+            message = savedInstanceState.getString(INPUT);
+            userOutput.setText("Hello, "+message+"!");
+        }
     }
 
+    public void actionBTN(View v){
+        message = userInput.getText().toString();
+        userOutput.setText("Hello, "+message+"!");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(INPUT, message);
+        super.onSaveInstanceState(outState);
+    }
 }
